88b0015156e7bf00f5ef845dc5ab1ebfef8cc1b3
Tim Morgan <tim@timmorgan.org>
2022-06-29 13:43:05 +0000

    Format code with clang-format (automated)

--- include/natalie_parser/node/for_node.hpp
+++ include/natalie_parser/node/for_node.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
+#include "natalie_parser/node/block_node.hpp"
 #include "natalie_parser/node/node.hpp"
 #include "natalie_parser/node/node_with_args.hpp"
-#include "natalie_parser/node/block_node.hpp"
 
 namespace NatalieParser {
 
@@ -29,14 +29,14 @@ public:
         creator->set_type("for");
         creator->append(m_expr);
         switch (m_vars->type()) {
-            case Node::Type::Identifier:
-                creator->with_assignment(true, [&]() { creator->append(*m_vars); });
-                break;
-            case Node::Type::MultipleAssignment:
-                creator->append(m_vars);
-                break;
-            default:
-                TM_UNREACHABLE();
+        case Node::Type::Identifier:
+            creator->with_assignment(true, [&]() { creator->append(*m_vars); });
+            break;
+        case Node::Type::MultipleAssignment:
+            creator->append(m_vars);
+            break;
+        default:
+            TM_UNREACHABLE();
         }
         if (!m_body->is_empty())
             creator->append(m_body->without_unnecessary_nesting());
